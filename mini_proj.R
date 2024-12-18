21c6bd0332b07866f2cce7f0f77642e490dd9d83
Erik Lillrank <erik.lillrank@gmail.com>
2024-12-18 15:35:28 +0100

    erik

--- mini_proj.R
+++ mini_proj.R
@@ -1,6 +1,9 @@
 library(keras)
 library(tensorflow)
-tensorflow::set_random_seed(1337)
+
+set.seed(1337)  # R seed
+tensorflow::set_random_seed(1337)  # TensorFlow seed
+Sys.setenv("PYTHONHASHSEED" = 1337)  # Python hash seed
 
 train_dir <- "C:/Users/erikl_xzy542i/Documents/Master_local/T3/MachineLearning/pneumonia/chest_xray/train"
 val_dir <- "C:/Users/erikl_xzy542i/Documents/Master_local/T3/MachineLearning/pneumonia/chest_xray/val"
@@ -19,16 +22,16 @@ train_datagen <- image_data_generator(
 )
 
 val_test_datagen <- image_data_generator(
-  rescale = 1/255 # Normalize pixel values for validation/testing
+  rescale = 1/255
 )
 
 # Training generator
 train_generator <- flow_images_from_directory(
   train_dir,
   generator = train_datagen,
-  target_size = c(128, 128),  # Resize images to match model input size
-  batch_size = 32,           # Number of images per batch
-  class_mode = "binary"      # Binary classification (normal vs pneumonia)
+  target_size = c(128, 128), 
+  batch_size = 32,           
+  class_mode = "binary"      
 )
 
 # Validation generator
@@ -47,7 +50,7 @@ test_generator <- flow_images_from_directory(
   target_size = c(128, 128),
   batch_size = 32,
   class_mode = "binary",
-  shuffle = FALSE            # Do not shuffle for testing (to align predictions with labels)
+  shuffle = FALSE      
 )
 
 
@@ -56,9 +59,9 @@ print(train_generator$class_indices)
 
 # Example: Visualize a batch of images
 batch <- generator_next(train_generator)
-par(mfrow = c(2, 3))  # Set plot grid (4 rows, 8 columns)
+par(mfrow = c(2, 3))  
 for (i in 1:6) {
-  img <- batch[[1]][i,,,]  # Extract image
+  img <- batch[[1]][i,,,]  
   plot(as.raster(img))
 }
 par(mfrow=c(1,1))
@@ -71,29 +74,35 @@ par(mfrow=c(1,1))
 # Basic CNN as our first model
 model <- keras_model_sequential() %>%
   # Convolutional layer 1
-  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu", input_shape = c(128, 128, 3)) %>%
+  layer_conv_2d(filters = 46, kernel_size = c(3, 3), activation = "relu", input_shape = c(128, 128, 3)) %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_dropout(rate = 0.2)%>%
   
   # Convolutional layer 2
-  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_dropout(rate = 0.2)%>%
   
   # Convolutional layer 3
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_conv_2d(filters = 256, kernel_size = c(3, 3), activation = "relu") %>%
   layer_max_pooling_2d(pool_size = c(2, 2)) %>%
   layer_dropout(rate = 0.2)%>%
   
   # Flatten and fully connected layers
   layer_flatten() %>%
-  layer_dense(units = 128, activation = "relu") %>%
+  layer_dense(units = 256, activation = "relu", kernel_regularizer = regularizer_l2(0.1)) %>%
   layer_dropout(rate = 0.5) %>% 
   layer_dense(units = 1, activation = "sigmoid") 
 
+# Early stopping scheme
+callback_early_stop <- callback_early_stopping(
+  monitor = "val_loss",
+  patience = 5,
+  restore_best_weights = TRUE
+)
 
 model %>% compile(
-  optimizer = optimizer_adam(learning_rate = 0.0001),
+  optimizer = optimizer_adam(learning_rate = 5e-5),
   loss = "binary_crossentropy",
   metrics = c("accuracy")
 )
@@ -102,12 +111,14 @@ model %>% compile(
 history <- model %>% fit(
   train_generator,
   steps_per_epoch = as.integer(train_generator$samples / train_generator$batch_size),
-  epochs = 10,
+  epochs = 50,
   validation_data = val_generator,
-  validation_steps = as.integer(val_generator$samples / val_generator$batch_size)
+  validation_steps = as.integer(val_generator$samples / val_generator$batch_size),
+  callbacks = list(callback_early_stop)
 )
-
 plot(history)
+tail(history$metrics$val_loss,1)
+tail(history$metrics$val_accuracy,1)
 
 results <- model %>% evaluate(
   test_generator,
@@ -132,15 +143,15 @@ base_model <- application_densenet121(
 freeze_weights(base_model)
 
 # Add top layers
-model <- keras_model_sequential() %>%
+model2 <- keras_model_sequential() %>%
   base_model %>%
   layer_flatten() %>%
-  layer_dense(units = 256, activation = 'relu', kernel_regularizer = regularizer_l2(0.01)) %>%
+  layer_dense(units = 512, activation = 'relu', kernel_regularizer = regularizer_l2(0.01)) %>%
   layer_dropout(0.5) %>%
   layer_dense(units = 1, activation = 'sigmoid')  
 
 
-model %>% compile( optimizer_adam(learning_rate = 1e-5), loss = 'binary_crossentropy', metrics = c('accuracy'))
+model2 %>% compile(optimizer_adam(learning_rate = 1e-5), loss = 'binary_crossentropy', metrics = c('accuracy'))
 
 # Early stopping scheme
 callback_early_stop <- callback_early_stopping(
@@ -150,21 +161,23 @@ callback_early_stop <- callback_early_stopping(
 )
 
 # Unfreeze trainable layers
-model$trainable <- TRUE
+model2$trainable <- TRUE
 
 # Fine-tune the model to data
-history_fine <- model %>% fit(
+history_fine <- model2 %>% fit(
   train_generator,
   steps_per_epoch = train_generator$samples %/% train_generator$batch_size,
-  epochs = 30,
+  epochs = 50,
   validation_data = val_generator,
   validation_steps = val_generator$samples %/% val_generator$batch_size,
   callbacks = list(callback_early_stop)
 )
 plot(history_fine)
+tail(history_fine$metrics$val_loss,1)
+tail(history_fine$metrics$val_accuracy,1)
 
 # Evaluate against test data
-results <- model %>% evaluate(
+results <- model2 %>% evaluate(
   test_generator,
   steps = as.integer(test_generator$samples / test_generator$batch_size)
 )
